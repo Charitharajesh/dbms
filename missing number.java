cd2312fc57586c830191c6ce0757e083086c0ebb
Charitharajesh <155131231+Charitharajesh@users.noreply.github.com>
2024-07-31 12:43:37 +0530

    Delete missing number.java

--- missing number.java
+++
@@ -1,8 +0,0 @@
-public class MissingNumber {
-    public static void main(String[] args) {
-        int[] a = {1, 4, 5, 3, 7, 8, 6};
-        int n = 8, totalSum = n * (n + 1) / 2, arraySum = 0;
-        for (int num : a) arraySum += num;
-        System.out.println("The missing number is: " + (totalSum - arraySum));
-    }
-}
